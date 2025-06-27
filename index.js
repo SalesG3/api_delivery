b917857b4d048544f8426180113deae09fd2a463
Sales <contabilgs3@gmail.com>
2025-06-27 16:15:59 -0300

    update

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const {app, con} = require('./server.js')
 
-app.get('/', async(req, res) => {
+app.get('/datasnap/entidade', async(req, res) => {
 
     let [data] = await con.promise().query('SELECT * FROM ENTIDADE');
 
     res.status(200).send(data);
 });
+
+app.get('/datasnap/produtos', async(req, res) => {
+
+    let [data] = await con.promise().query('SELECT * FROM PRODUTOS');
+
+    res.status(200).send(data);
+});
+
+app.post('/datasnap/caio', function(req, res){
+    
+    if(req.body.PARAMETRO == "Caio"){
+        res.send({
+            teste: "PASSOU"
+        })
+        return
+    }
+
+    res.send({
+        teste:"NÃO!"
+    })
+
+
+
+})

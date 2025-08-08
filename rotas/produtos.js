7ca615ab775066e767b00e85c54aa3c2919d0ac0
Sales <contabilgs3@gmail.com>
2025-08-08 09:46:58 -0300

    :rocket: feat: nova rota para exclusão de produtos e ajuste na execução da rota de inserção

--- rotas/produtos.js
+++ rotas/produtos.js
@@ -19,7 +19,7 @@ app.post('/importData/produto', upload.single('img'), async(req, res) => {
 
     let json = JSON.parse(req.body.json)
 
-    let { CD_PRODUTO, NM_PRODUTO, DS_PRODUTO, VL_PRODUTO, ID_CATEGORIA } = json.dataRows
+    let { CD_PRODUTO, NM_PRODUTO, DS_PRODUTO, VL_PRODUTO, ID_CATEGORIA } = json
 
     if(!CD_PRODUTO || !NM_PRODUTO || !VL_PRODUTO || !ID_CATEGORIA){
         res.status(400).send({
@@ -31,9 +31,9 @@ app.post('/importData/produto', upload.single('img'), async(req, res) => {
     try{
 
         // Execução da procedure de inserção
-        await connect.promise().beginTransaction()
+        await connect.beginTransaction()
 
-        let [data] = await connect.promise().execute(`CALL NOVO_PRODUTO ( ?, ?, ?, ?, ?)`,
+        let [data] = await connect.execute(`CALL NOVO_PRODUTO ( ?, ?, ?, ?, ?)`,
             [CD_PRODUTO, NM_PRODUTO, DS_PRODUTO, VL_PRODUTO, ID_CATEGORIA]
         )
 
@@ -44,11 +44,11 @@ app.post('/importData/produto', upload.single('img'), async(req, res) => {
 
             let url = await uploadImg(img_path, data[0][0].ID_PRODUTO)
 
-            let [update] = await connect.promise().execute(`UPDATE PRODUTOS SET IMG_PRODUTO = ? WHERE ID_PRODUTO = ?`,
+            let [update] = await connect.execute(`UPDATE PRODUTOS SET IMG_PRODUTO = ? WHERE ID_PRODUTO = ?`,
                 [url, data[0][0].ID_PRODUTO]
             )
 
-            await connect.promise().commit()
+            await connect.commit()
             connect.release()
 
             fs.unlink(img_path, (err) => {
@@ -63,7 +63,7 @@ app.post('/importData/produto', upload.single('img'), async(req, res) => {
             return
         }
 
-        await connect.promise().commit()
+        await connect.commit()
         connect.release()
 
         res.status(200).send({
@@ -82,7 +82,7 @@ app.post('/importData/produto', upload.single('img'), async(req, res) => {
         }
 
         // Tratamento de erros banco de dados
-        await connect.promise().rollback()
+        await connect.rollback()
         connect.release()
         
 
@@ -97,5 +97,52 @@ app.post('/importData/produto', upload.single('img'), async(req, res) => {
                 error: err
             })
         }
+    }
+})
+
+// Rota para deletar produto
+app.delete('/deleteData/produto/:id', async(req, res) => {
+
+    // Validaçõs Iniciais
+    if(!req.params.id){
+        res.status(400).send({
+            error: "Parâmetros esperados não encontrados!"
+        })
+        return
+    }
+
+    // Execução no Banco
+    try{
+
+        let [data] = await con.promise().execute(`DELETE FROM PRODUTOS WHERE ID_PRODUTO = ?`,
+            [req.params.id]
+        )
+
+        if(data.affectedRows < 1){
+            res.status(400).send({
+                error: `Chave Inexistente! CATEGORIA.ID_CATEGORIA(${req.params.id})`
+            })
+            return
+        }
+
+        res.status(200).send({
+            sucesso: "Registro deletado do banco de dados!"
+        })
+        return
+    }
+
+    // Tratamento de erros
+    catch(err){
+        if(err.code == "ER_ROW_IS_REFERENCED_2"){
+            res.status(400).send({
+                erro: "Não foi possível excluir pois o registro possui dependências!"
+            })
+            return
+        }
+
+        res.status(500).send({
+            erro: err
+        })
+        return
     }
 })
